5cd05612fb247be9735c58e3bb4312d7d35e25b6
jfjhh <alex.striff1@gmail.com>
2018-05-21 14:45:30 -0700

    Bar color hack

--- dwm.c
+++ dwm.c
@@ -819,14 +819,15 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
+	/* HACK: I don't like how the majority of the bar is SchemeSel, so always
+	 * use SchemeNorm for this part. */
+	drw_setscheme(drw, scheme[SchemeNorm]);
 	if ((w = m->ww - sw - x) > bh) {
 		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
